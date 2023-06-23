c44448895bfd3f1deef5414d345f3b95bf15b3bb
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 <esmeii>
2023-06-23 20:01:43 +0900

    rdma report update

--- timing/rdma/rdmaengine.go
+++ timing/rdma/rdmaengine.go
@@ -1,10 +1,11 @@
-// Package rdma provides the implementation of an RDMA engine.
+//Package rdma provides the implementation of an RDMA engine.
 package rdma
 
 import (
 	"log"
 	"reflect"
-
+	"fmt"
+	"os"
 	"gitlab.com/akita/akita/v3/sim"
 	"gitlab.com/akita/akita/v3/tracing"
 	"gitlab.com/akita/mem/v3/mem"
@@ -15,6 +16,9 @@ type transaction struct {
 	fromOutside sim.Msg
 	toInside    sim.Msg
 	toOutside   sim.Msg
+	sendTime	sim.VTimeInSec
+	recvTime	sim.VTimeInSec
+	addr	 uint64
 }
 
 // An Engine is a component that helps one GPU to access the memory on
@@ -38,6 +42,11 @@ type Engine struct {
 
 	transactionsFromOutside []transaction
 	transactionsFromInside  []transaction
+	transactionEpoch        sim.VTimeInSec
+	transactionEpochRsp	sim.VTimeInSec
+	srcUsageCount       map[string]int
+	dstUsageCount       map[string]int
+	dstGPU                  []string
 }
 
 // SetLocalModuleFinder sets the table to lookup for local data.
@@ -115,6 +124,69 @@ func (e *Engine) drainRDMA(now sim.VTimeInSec) bool {
 	}
 	return false
 }
+//EUN
+func (e *Engine) writeFileRdmaFromInside(req mem.AccessRsp, epoch sim.VTimeInSec) {
+	f, err := os.OpenFile("rdma.log", os.O_APPEND|os.O_RDWR, 0755)
+        if err != nil {
+        // Handle the error, such as creating the file if it doesn't exist
+       	if os.IsNotExist(err) {
+        	f, err = os.Create("rdma.log")
+                        if err != nil {
+                        	log.Fatal(err)
+                        }
+             	} else {
+                	log.Fatal(err)
+                	}
+        }
+	defer f.Close()
+	fmt.Fprintf(f, "---------------------------------------[Transactions Resolved]-------------------------------------------\n")
+	fmt.Fprintf(f,"send time=%f\tsrc=\t%s\tdst=\t%s\tAddr= %s\tepoch time= %f\n\n",req.Meta().SendTime,req.Meta().Src.Name(),req.Meta().Dst.Name(),req.Meta().ID,epoch)
+}
+
+
+//ES
+func (e *Engine) countRequestUsage(trans transaction) {
+	src := trans.fromInside.Meta().Src.Name()
+	dst := trans.toOutside.Meta().Dst.Name()
+
+	// Count usage as src
+	if _, ok := e.srcUsageCount[src]; ok {
+		e.srcUsageCount[src]++
+	} else {
+		e.srcUsageCount[src] = 1
+	}
+
+	// Count usage as dst
+	if _, ok := e.dstUsageCount[dst]; ok {
+		e.dstUsageCount[dst]++
+	} else {
+		e.dstUsageCount[dst] = 1
+	}
+	 f, err := os.OpenFile("./rdma.log", os.O_APPEND|os.O_RDWR, 0755)
+                if err != nil {
+        // Handle the error, such as creating the file if it doesn't exist
+                        if os.IsNotExist(err) {
+                                f, err = os.Create("rdma.log")
+                                if err != nil {
+                                        log.Fatal(err)
+                                }
+                        } else {
+                                log.Fatal(err)
+                        }
+                }
+                defer f.Close()
+                fmt.Fprintf(f,"[Request Info]\n")
+                fmt.Fprintf(f,"Send time= %f\tsrc=\t%s\tdst=\t%s\tAddr= %d\n",trans.sendTime,trans.fromInside.Meta().Src.Name(),trans.toOutside.Meta().Dst.Name(),trans.addr)
+		fmt.Fprintf(f,"\n[SRC usage Count]\n")
+		for src, count := range e.srcUsageCount {
+			fmt.Fprintf(f,"[%s]: %d\n", src, count)
+		}
+		fmt.Fprintf(f,"\n[DST usage Count]\n")
+		for dst, count := range e.dstUsageCount {
+			fmt.Fprintf(f,"[%s]: %d\n", dst, count)
+		}
+
+}
 
 func (e *Engine) fullyDrained() bool {
 	return len(e.transactionsFromOutside) == 0 &&
@@ -185,7 +257,7 @@ func (e *Engine) processReqFromL1(
 	cloned.Meta().Src = e.ToOutside
 	cloned.Meta().Dst = dst
 	cloned.Meta().SendTime = now
-
+	e.transactionEpoch = now
 	err := e.ToOutside.Send(cloned)
 	if err == nil {
 		e.ToL1.Retrieve(now)
@@ -193,14 +265,14 @@ func (e *Engine) processReqFromL1(
 		tracing.TraceReqReceive(req, e)
 		tracing.TraceReqInitiate(cloned, e, tracing.MsgIDAtReceiver(req, e))
 
-		//fmt.Printf("%s req inside %s -> outside %s\n",
-		//e.Name(), req.GetID(), cloned.GetID())
-
 		trans := transaction{
 			fromInside: req,
 			toOutside:  cloned,
+			sendTime: now,
+			addr: req.GetAddress(),
 		}
 		e.transactionsFromInside = append(e.transactionsFromInside, trans)
+		e.countRequestUsage(trans)
 
 		return true
 	}
@@ -219,15 +291,13 @@ func (e *Engine) processReqFromOutside(
 	cloned.Meta().Dst = dst
 	cloned.Meta().SendTime = now
 
+	e.transactionEpochRsp = now
 	err := e.ToL2.Send(cloned)
 	if err == nil {
 		e.ToOutside.Retrieve(now)
 
 		tracing.TraceReqReceive(req, e)
 		tracing.TraceReqInitiate(cloned, e, tracing.MsgIDAtReceiver(req, e))
-
-		//fmt.Printf("%s req outside %s -> inside %s\n",
-		//e.Name(), req.GetID(), cloned.GetID())
 
 		trans := transaction{
 			fromOutside: req,
@@ -253,12 +323,11 @@ func (e *Engine) processRspFromL2(
 	rspToOutside.Meta().Src = e.ToOutside
 	rspToOutside.Meta().Dst = trans.fromOutside.Meta().Src
 
+	fmt.Printf("src= %s\t->dst= %s\n",rspToOutside.Meta().Src.Name(),rspToOutside.Meta().Dst.Name())
+	e.transactionEpoch = 0	
 	err := e.ToOutside.Send(rspToOutside)
 	if err == nil {
 		e.ToL2.Retrieve(now)
-
-		//fmt.Printf("%s rsp inside %s -> outside %s\n",
-		//e.Name(), rsp.GetID(), rspToOutside.GetID())
 
 		tracing.TraceReqFinalize(trans.toInside, e)
 		tracing.TraceReqComplete(trans.fromOutside, e)
@@ -278,12 +347,19 @@ func (e *Engine) processRspFromOutside(
 	transactionIndex := e.findTransactionByRspToID(
 		rsp.GetRspTo(), e.transactionsFromInside)
 	trans := e.transactionsFromInside[transactionIndex]
-
+	trans.recvTime = now
+	
 	rspToInside := e.cloneRsp(rsp, trans.fromInside.Meta().ID)
 	rspToInside.Meta().SendTime = now
 	rspToInside.Meta().Src = e.ToL1
 	rspToInside.Meta().Dst = trans.fromInside.Meta().Src
-
+	
+	e.transactionEpoch = now - e.transactionEpoch
+	e.transactionEpochRsp = now - e.transactionEpochRsp
+	fmt.Printf("transaction epoch between 2 and 4= %f\n",e.transactionEpochRsp)
+	e.transactionEpochRsp = 0
+	e.writeFileRdmaFromInside(rspToInside,e.transactionEpoch)
+	e.transactionEpoch = 0
 	err := e.ToL1.Send(rspToInside)
 	if err == nil {
 		e.ToOutside.Retrieve(now)
@@ -291,19 +367,14 @@ func (e *Engine) processRspFromOutside(
 		tracing.TraceReqFinalize(trans.toOutside, e)
 		tracing.TraceReqComplete(trans.fromInside, e)
 
-		//fmt.Printf("%s rsp outside %s -> inside %s\n",
-		//e.Name(), rsp.GetID(), rspToInside.GetID())
-
 		e.transactionsFromInside =
 			append(e.transactionsFromInside[:transactionIndex],
 				e.transactionsFromInside[transactionIndex+1:]...)
-
 		return true
 	}
 
 	return false
 }
-
 func (e *Engine) findTransactionByRspToID(
 	rspTo string,
 	transactions []transaction,
@@ -393,6 +464,8 @@ func NewEngine(
 	e.localModules = localModules
 	e.RemoteRDMAAddressTable = remoteModules
 
+	e.srcUsageCount = make(map[string]int)
+	e.dstUsageCount = make(map[string]int)
 	e.ToL1 = sim.NewLimitNumMsgPort(e, 1, name+".ToL1")
 	e.ToL2 = sim.NewLimitNumMsgPort(e, 1, name+".ToL2")
 	e.CtrlPort = sim.NewLimitNumMsgPort(e, 1, name+".CtrlPort")
