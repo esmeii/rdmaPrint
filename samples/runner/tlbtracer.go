99631e7411dbca8fff15ec4151a2762fb4b38717
esmeii <eunseong@ewhain.net>
2023-07-13 22:05:50 +0900

    tlb

--- samples/runner/tlbtracer.go
+++ samples/runner/tlbtracer.go
@@ -12,6 +12,8 @@ type tlbTracer struct {
 	traceWriter *tracing.CSVTraceWriter
 }
 
+var taskCount uint64 = 0
+
 // newTLBTracer creates a tracer for TLB access.
 func newTlbTracer(traceWriter *tracing.CSVTraceWriter) *tlbTracer {
 	return &tlbTracer{
@@ -32,33 +34,31 @@ func (t *tlbTracer) EndTask(task tracing.Task) {
 }
 
 // TraceTLBAccess records the TLB access information.
-func TraceTLBAccess(
+func (t *tlbTracer) TraceTLBAccess(
 	req *vm.TranslationReq,
 	setID int,
 	wayID int,
 	vAddr uint64,
 	traceWriter *tracing.CSVTraceWriter,
 ) {
+	taskCount++
+	taskID := fmt.Sprintf("TLB-Access-%d", taskCount)
+
 	task := tracing.Task{
-		ID:        tracing.NextID(),
+		ID:        taskID,
 		ParentID:  "",
 		Kind:      "TLB Access",
 		What:      "TLB Access",
 		Where:     "",
 		StartTime: 0,
 		EndTime:   0,
-		Detail: &tlbTracer{
-			traceWriter: traceWriter,
-		},
 	}
-
 	traceWriter.Write(task)
 
-	tlbTracer := task.Detail.(*tlbTracer)
-
-	tlbTracer.traceWriter.Write(tracing.Task{
-		ID:        tracing.NextID(),
-		ParentID:  task.ID,
+	lookupTaskID := fmt.Sprintf("%s-Lookup", taskID)
+	traceWriter.Write(tracing.Task{
+		ID:        lookupTaskID,
+		ParentID:  taskID,
 		Kind:      "Lookup",
 		What:      strconv.FormatUint(req.VAddr, 10),
 		Where:     fmt.Sprintf("SetID: %d, WayID: %d", setID, wayID),
